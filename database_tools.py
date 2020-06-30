e5a35d865cc7b28666be7dbce997464effbb6f37
vonunwerth <maximilian@von-unwerth.de>
2020-06-30 22:45:14 +0200

    Warnings and errors, when questions and answers are to long. Only continues, if there are 0 errors. Warning can be ignored but for example show missing citations, missing questions mark, ...

--- database_tools.py
+++ database_tools.py
@@ -5,6 +5,8 @@
 # TODO function which can repair corrupted entries
 import sqlite3
 
+import database_statistics
+
 
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
@@ -21,19 +23,91 @@ def validate_questions():
     with conn:
         cur = conn.cursor()
         cur.execute("SELECT id,question FROM qac ORDER BY id")
+
+        error_count = 0
+        warning_count = 0
         for entry in cur.fetchall():
             question_id = entry[0]
             question = entry[1]
-            if len(question) > 60:
-                print "\033[91mError: ID: " + str(question_id) + " is to long! Cards will be ugly! \033[0m" + question
+            any_error = False
+            if len(question) > 120:
+                print "\033[91mError: Question of entry with ID: " + str(question_id) + " is to long! Cards will be ugly! \033[0m" + question
+                error_count = error_count + 1
+                any_error = True
             if question[-1] != "?":
+                any_error = True
+                warning_count = warning_count + 1
                 if question[-1] == " ":
-                    print "\033[93mWarning: ID: " + str(
+                    print "\033[93mWarning: Question of entry with ID: " + str(
                         question_id) + " has spaces attached to the back. \033[0m" + question
                 else:
-                    print "\033[93mWarning: ID: " + str(question_id) + " has not a ? as last symbol! \033[0m" + question
-            print("\n")
+                    print "\033[93mWarning: Question of entry with ID: " + str(question_id) + " has not a ? as last symbol! \033[0m" + question
+            if any_error:
+                print("")  # Organize Errors and Warnings in blocks grouped by id
+
+        print "\033[1mQuestion-Report\n\033[0m\033[91m  Errors found: \033[0m\033[1m" + str(
+            error_count) + "\n \033[0m\033[93m Warnings found: \033[0m\033[1m" + str(warning_count) + "\033[0m\n"
+        return (warning_count, error_count)
+
+
+def validate_answers():
+    print "Validating answer entries..."
+    conn = create_connection("python_sqlite.db")
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT id,answer,category FROM qac ORDER BY id")
+        error_count = 0
+        warning_count = 0
+        for entry in cur.fetchall():
+            answer_id = entry[0]
+            answer = entry[1]
+            answer_cat = entry[2]
+            any_error = False
+            if len(answer.split("(")[0]) > 60:
+                print "\033[91mError: Answer of entry with ID: " + str(answer_id) + " is to long! Cards will be ugly! \033[0m" + answer
+                error_count = error_count + 1
+                any_error = True
+            if answer_cat != "T" and (
+                    answer.find("(") < 0 or answer.find(")") < 0):  # TODO just for this Star Trek thingy thing
+                any_error = True
+                warning_count = warning_count + 1
+                print "\033[93mWarning: Answer of entry with ID: " + str(
+                    answer_id) + " is missing correct citation and is not of category: Trivia \033[0m" + answer  # TODO not everyone will use citations!
+            if any_error:
+                print("")  # Organize Errors and Warnings in blocks grouped by id
+
+        print "\033[1mAnswer-Report\n\033[0m\033[91m  Errors found: \033[0m\033[1m" + str(
+            error_count) + "\n \033[0m\033[93m Warnings found: \033[0m\033[1m" + str(warning_count) + "\033[0m\n"
+        return (warning_count, error_count)
+
+
+def validate_categories():
+    print "Validating category entries..."
+    conn = create_connection("python_sqlite.db")
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT id,question,category FROM qac ORDER BY id")
+        error_count = 0
+        warning_count = 0
+        for entry in cur.fetchall():
+            question_id = entry[0]
+            question = entry[1]
+            category = entry[2]
+            any_error = False
+            if category not in database_statistics.get_categories_from_file()[0]:
+                any_error = True
+                error_count = error_count + 1
+                print "\033[91mError: Category of entry with ID: " + str(
+                    question_id) + " is missing a correct category \033[0m Current Category: " + category
+            if any_error:
+                print("")  # Organize Errors and Warnings in blocks grouped by id
+
+        print "\033[1mCategory-Report\n\033[0m\033[91m  Errors found: \033[0m\033[1m" + str(
+            error_count) + "\n \033[0m\033[93m Warnings found: \033[0m\033[1m" + str(warning_count) + "\033[0m\n"
+        return (warning_count, error_count)
 
 
 if __name__ == '__main__':
     validate_questions()
+    validate_answers()
+    validate_categories()
--- trivial_pursuit_creator.py
+++ trivial_pursuit_creator.py
@@ -1,4 +1,5 @@
 import database_statistics as dbs
+import database_tools as dt
 import questions_to_database as qtb
 import create_cards as cc
 import print_cards as pc
@@ -8,14 +9,17 @@ print "The questions are taken from a questions.txt file from the folder questio
 print "Please specify also the categories in a file called categories.txt in the same folder."
 raw_input("Press \033[1mENTER\033[0m to start adding new questions from ./questions/questions.txt to the database.\n"
           "Already inserted questions will be ignored (The questions text must be unique.)")
-
 qtb.questions_to_database()
-raw_input("Press \033[1mENTER\033[0m to continue and show available statistics on how much cards can be created.")
-dbs.database_statistics()
-raw_input("Press \033[1mENTER\033[0m to create questions cards (front and back) in the ./out folder")
-cc.create_cards()
-raw_input(
-    "Press \033[1mENTER\033[0m to create printable sheets with 4 questions card, which can be cut, kinked and glued "
-    "together.")
-pc.print_cards()
-print "Congratulations. You can find the result in ./prints."
+raw_input("Press \033[1mENTER\033[0m to start validating the database.")
+if dt.validate_questions() == 0 and dt.validate_answers() == 0 and dt.validate_categories() == 0:
+    raw_input("Press \033[1mENTER\033[0m to continue and show available statistics on how much cards can be created.")
+    dbs.database_statistics()
+    raw_input("Press \033[1mENTER\033[0m to create questions cards (front and back) in the ./out folder")
+    cc.create_cards()
+    raw_input(
+        "Press \033[1mENTER\033[0m to create printable sheets with 4 questions card, which can be cut, kinked and glued "
+        "together.")
+    pc.print_cards()
+    print "Congratulations. You can find the result in ./prints."
+else:
+    print "Card creation aborted. Please fix the errors in your database and start again."
